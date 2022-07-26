10af9406ae31c8ae9b569e75d7ea309e493f61a5
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-07-25 23:20:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Password Generator
+
+## Description
+
+The goal of the project was to modify the starter code to create an application that enables clients to generate random passwords based on criteria that they’ve selected. The client can chose to generate the password using between 8 and 128 characters: lower cased characters, upper cased characters, numbers and special characters.😀
+## Table of Contents 
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation
+
+Clone the repository to your local machine and open the index.html file in your browser
+
+## Usage
+
+[Deployed Site](https://andresilva8624.github.io/first-assignment/)
+
+![gif of deployed site](./assets/images/website.gif)
+   
+
+## Credits
+
+Thank you to [Sabrina Hanson](https://www.github.com/sabhanson) for helping me with this assignment
+
+## License
+
+MIT License
+
+Copyright (c) 2022 Andre Silva
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Contact
+To contact me use one of the links below
+
+
+[GitHub](https://www.github.com/andresilva8624)
+
+
+
+[EMail](mailto:andresilva8624@gmail.com)
---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// var password = document.getElementById("password");
+
+var password = document.querySelector("password");
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialCharacters = [
+  "@",
+  "%",
+  ",",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function generatePassword() {
+  var length = parseInt(prompt("How long do you want your password to be?"));
+  var passwordOptions = [];
+  var finalPassword = [];
+  console.log(length);
+
+  if (length < 8 || length > 128) {
+    alert("Password needs to be of valid length");
+    return;
+  }
+  var hasNumbers = confirm("Do you want to include numbers in your password?");
+  var hasLowerCase = confirm(
+    "Do you want to include lower case characters in your password?"
+  );
+  var hasUpperCase = confirm(
+    "Do you want to include upper case characters in your password?"
+  );
+  var hasSpecialCharacters = confirm(
+    "Do you want to include special characters in your password?"
+  );
+  if (hasNumbers) {
+    passwordOptions = passwordOptions.concat(numericCharacters);
+  }
+  if (hasUpperCase) {
+    passwordOptions = passwordOptions.concat(upperCasedCharacters);
+  }
+  if (hasLowerCase) {
+    passwordOptions = passwordOptions.concat(lowerCasedCharacters);
+  }
+  if (hasSpecialCharacters) {
+    passwordOptions = passwordOptions.concat(specialCharacters);
+  } 
+  console.log(passwordOptions);
+
+  for (var i=0; i<length; i++){
+finalPassword.push(passwordOptions[Math.floor(Math.random() * passwordOptions.length)]);
+
+  };
+console.log(finalPassword);
+return finalPassword.join('');
+}
+generateBtn.addEventListener("click", writePassword);
